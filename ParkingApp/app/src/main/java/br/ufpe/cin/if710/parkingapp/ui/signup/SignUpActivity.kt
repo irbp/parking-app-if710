4603da210da4bbc60b9015f55a9d2f1e0bfdebb9
Ítalo Paulino <irbp@cin.ufpe.br>
2018-12-01 11:22:35 -0300

    Add basic activities

---
+++ ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/ui/signup/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package br.ufpe.cin.if710.parkingapp.ui.signup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.ufpe.cin.if710.parkingapp.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
