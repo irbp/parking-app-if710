92e19cd9bc81bb4fc1759ade3b7a27b87dee9f5c
fms6 <fms6@cin.ufpe.br>
2018-12-13 00:49:55 -0300

    Create preference activity

--- ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/ui/MainActivity.kt
+++ ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/ui/MainActivity.kt
@@ -3,17 +3,10 @@ package br.ufpe.cin.if710.parkingapp.ui
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import br.ufpe.cin.if710.parkingapp.R
-import br.ufpe.cin.if710.parkingapp.network.api.ApiService
-import br.ufpe.cin.if710.parkingapp.utils.inject
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
+
 
 class MainActivity : AppCompatActivity() {
-    private val ParkingApi by inject<ApiService>()
-    private var disposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
