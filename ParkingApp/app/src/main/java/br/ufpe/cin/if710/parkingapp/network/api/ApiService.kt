7c15609b1bd25bc04dab280cc32a9ceea75a024c
fms6 <fms6@cin.ufpe.br>
2018-12-12 22:14:41 -0300

    Use rest api as api service

--- ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/network/api/ApiService.kt
+++ ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/network/api/ApiService.kt
@@ -1,42 +1,48 @@
 package br.ufpe.cin.if710.parkingapp.network.api
 
 import android.app.Application
+import br.ufpe.cin.if710.parkingapp.R
+import br.ufpe.cin.if710.parkingapp.network.api.model.Parking
+import br.ufpe.cin.if710.parkingapp.utils.inject
 import io.reactivex.Observable
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
-
-import br.ufpe.cin.if710.parkingapp.R
-import br.ufpe.cin.if710.parkingapp.network.model.ParkingListResponse
-import br.ufpe.cin.if710.parkingapp.network.model.ParkingResponse
-import br.ufpe.cin.if710.parkingapp.utils.inject
+import retrofit2.http.Query
 
 
-interface FirestoreApiService {
+interface ApiService {
+    @GET("parkings")
+    fun getParkingInside(
+        @Query("lng") longitude: Number,
+        @Query("lat") latitude: Number): Observable<Parking>
 
-    @GET("parkings/")
-    fun getAllParkings(): Observable<ParkingListResponse>
+    @GET("parkings")
+    fun getAllParkingsAround(
+        @Query("lng") longitude: Number,
+        @Query("lat") latitude: Number,
+        @Query("radius") radius: Number): Observable<Array<Parking>>
 
     @GET("parkings/{id}")
-    fun getParkingById(@Path("id") id: String): Observable<ParkingResponse>
+    fun getParkingById(@Path("id") id: String): Observable<Parking>
 
     /**
-     * Companion object to create the Firestore API Service
+     * Companion object to create the API Service
      */
     companion object Factory {
         private val application by inject<Application>()
         private val apiBaseUrl = application.getString(R.string.api_base_url)
 
-        fun create(): FirestoreApiService {
+        fun create(): ApiService {
             val retrofit = Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(apiBaseUrl)
                 .build()
 
-            return retrofit.create(FirestoreApiService::class.java)
+            return retrofit.create(ApiService::class.java)
         }
     }
 
--- ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/ui/MainActivity.kt
+++ ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/ui/MainActivity.kt
@@ -3,14 +3,39 @@ package br.ufpe.cin.if710.parkingapp.ui
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import br.ufpe.cin.if710.parkingapp.R
+import br.ufpe.cin.if710.parkingapp.network.api.ApiService
+import br.ufpe.cin.if710.parkingapp.utils.inject
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    private val ParkingApi by inject<ApiService>()
+    private var disposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this@MainActivity, ParkingListActivity::class.java))
+        Log.d("Result", "Result is chegou")
+
+        disposable = ParkingApi.getAllParkingsAround(8, 8, 2000)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe ({
+                    result ->
+                Log.d("Result", "Result is $result")
+            }, { error ->
+                Log.d("Result", "error is ${error.message}")
+            })
+
+//        startActivity(Intent(this@MainActivity, ParkingListActivity::class.java))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disposable?.dispose()
     }
 }
