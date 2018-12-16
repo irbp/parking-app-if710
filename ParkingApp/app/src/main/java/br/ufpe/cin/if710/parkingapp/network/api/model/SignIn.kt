dcacb8a5fc56460798d7d1b12e9a1d87a2e8acda
fms6 <fms6@cin.ufpe.br>
2018-12-16 16:28:11 -0300

    Add authentication and redirect activities

--- ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/network/api/model/SignIn.kt
+++ ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/network/api/model/SignIn.kt
@@ -2,13 +2,13 @@ package br.ufpe.cin.if710.parkingapp.network.api.model
 
 import com.google.gson.annotations.SerializedName
 
-data class SignUpRequest(
-    @SerializedName("name") val name: String,
+
+data class SignInRequest(
     @SerializedName("email") val username: String,
     @SerializedName("password") val password: String
 )
 
-data class SignUpResponse(
+data class SignInResponse(
     @SerializedName("username") val username: String,
     @SerializedName("name") val name: String,
     @SerializedName("password") val password: String
--- ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/ui/SignInActivity.kt
+++ ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/ui/SignInActivity.kt
@@ -1,23 +1,50 @@
 package br.ufpe.cin.if710.parkingapp.ui
 
-import android.support.v7.app.AppCompatActivity
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import br.ufpe.cin.if710.parkingapp.R
 import br.ufpe.cin.if710.parkingapp.Utils
-import br.ufpe.cin.if710.parkingapp.network.api.Session
-import br.ufpe.cin.if710.parkingapp.utils.inject
+import br.ufpe.cin.if710.parkingapp.viewmodel.SignInViewModel
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import org.jetbrains.anko.toast
 
 class SignInActivity : AppCompatActivity() {
-    private val session: Session by inject<Session>()
+    private lateinit var viewModel: SignInViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        Utils.bindValidator(et_signin_email, Utils.emailValidator)
-        Utils.bindValidator(et_signin_password, Utils.passwordValidator)
+        viewModel = ViewModelProviders.of(this).get(SignInViewModel::class.java)
+
+        Utils.bindValidators(et_signin_email, arrayOf(Utils.notEmptyValidator, Utils.emailValidator))
+        Utils.bindValidators(et_signin_password, arrayOf(Utils.notEmptyValidator, Utils.minLengthValidator(6)))
+
+        Utils.disableIfInvalid(btn_signin_login, arrayOf(et_signin_email, et_signin_password))
+
+        txt_signin_create_account.setOnClickListener {
+            startActivity(Intent(this@SignInActivity, SignUpActivity::class.java))
+        }
+
+        btn_signin_login.setOnClickListener {
+            viewModel.signIn(et_signin_email.text, et_signin_password.text)
+                .subscribe(
+                    { response ->
+                        if (response.isSuccessful) {
+                            startActivity(Intent(this@SignInActivity, ParkingListActivity::class.java))
+                        } else {
+                            toast(response.code())
+                        }
+                    },
+                    {
+                        toast(getString(R.string.user_signin_error))
+                    }
+                )
+        }
+
     }
 
 }
