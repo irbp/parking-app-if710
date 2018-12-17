30b85c879df688f3222faefa89bfa73fcd751ae0
Ítalo Paulino <irbp@cin.ufpe.br>
2018-12-17 08:44:15 -0300

    Add image folder

--- ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/ui/SuggestParkingActivity.kt
+++ ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/ui/SuggestParkingActivity.kt
@@ -1,6 +1,5 @@
 package br.ufpe.cin.if710.parkingapp.ui
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import br.ufpe.cin.if710.parkingapp.R
