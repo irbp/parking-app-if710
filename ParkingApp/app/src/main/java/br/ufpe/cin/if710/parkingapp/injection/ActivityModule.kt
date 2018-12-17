e53f5b6137690a2f71c01ec90878534579278ea2
fms6 <fms6@cin.ufpe.br>
2018-12-17 08:18:40 -0300

    Add daggle 2 injection classes

--- ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/injection/ActivityModule.kt
+++ ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/injection/ActivityModule.kt
@@ -1,2 +1,30 @@
 package br.ufpe.cin.if710.parkingapp.injection
 
+import br.ufpe.cin.if710.parkingapp.ui.MainActivity
+import br.ufpe.cin.if710.parkingapp.ui.ParkingListActivity
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+
+/**
+ * TODO:
+ * - Implement 
+ *
+ * All activities intended to use Dagger2 @Inject should be listed here.
+ *
+ * Example:
+ *
+ * @ContributesAndroidInjector
+ * internal abstract fun contributeFooActivity(): FooActivity
+ *
+ */
+@Module
+abstract class ActivityModule {
+
+    @ContributesAndroidInjector
+    internal abstract fun contributeMainActivity(): MainActivity
+
+    @ContributesAndroidInjector
+    internal abstract fun contributeParkingListActivity(): ParkingListActivity
+
+}
--- ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/injection/AppModule.kt
+++ ParkingApp/app/src/main/java/br/ufpe/cin/if710/parkingapp/injection/AppModule.kt
@@ -1,2 +1,34 @@
 package br.ufpe.cin.if710.parkingapp.injection
 
+import android.app.Application
+import br.ufpe.cin.if710.parkingapp.db.ParkingAppDatabase
+import br.ufpe.cin.if710.parkingapp.db.dao.ParkingDetailsDao
+import br.ufpe.cin.if710.parkingapp.db.dao.UserDao
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+/**
+ * AppModule shall be used initialize objects used across our application,
+ * such as Room(Database), Retrofit, Shared Preference, etc.
+ */
+@Module
+internal class AppModule {
+    @Singleton
+    @Provides
+    fun provideDatabase(app: Application): ParkingAppDatabase {
+        return ParkingAppDatabase.getInstance(app)
+    }
+
+    @Singleton
+    @Provides
+    fun provideUserDao(db: ParkingAppDatabase): UserDao {
+        return db.userDao()
+    }
+
+    @Singleton
+    @Provides
+    fun provideParkingDetailsDao(db: ParkingAppDatabase): ParkingDetailsDao {
+        return db.parkingDetailsDao()
+    }
+}
